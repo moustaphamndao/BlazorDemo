d6e4b9af0d5de3cc421fb7b9b3d793633047089b
Taphagol <tc1ndao@yahoo.fr>
2020-05-06 18:38:43 -0400

    Implementing Blazor event handling

--- EmployeeManagement.Web/Pages/EmployeeDetailsBase.cs
+++ EmployeeManagement.Web/Pages/EmployeeDetailsBase.cs
@@ -1,9 +1,11 @@
 ﻿using EmployeeManagement.Models;
 using EmployeeManagement.Web.Services;
 using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Permissions;
 using System.Threading.Tasks;
 
 namespace EmployeeManagement.Web.Pages
@@ -11,6 +13,10 @@ namespace EmployeeManagement.Web.Pages
     public class EmployeeDetailsBase : ComponentBase
     {
         public Employee employee { get; set; } = new Employee();
+        protected string Coordinates { get; set; }
+        protected string ButtonText { get; set; } = "Hide Footer";
+        protected string CssClass { get; set; } = null;
+
 
         [Inject]
         public IEmployeeService employeeService { get; set; }
@@ -22,6 +28,26 @@ namespace EmployeeManagement.Web.Pages
         {
             Id = Id ?? "1"; // In the case the value of Id is not in the URL, Use the empployee with Id 1.
             employee = await employeeService.GetEmployee(int.Parse(Id));
+        }
+
+        protected void Mouse_Move(MouseEventArgs e)
+        {
+            Coordinates = $"X = {e.ClientX} Y = {e.ClientY}";
+        }
+
+        protected void Button_Click()
+        {
+            if(ButtonText == "Hide Footer")
+            {
+                ButtonText = "Show Footer";
+                CssClass = "HideFooter";
+            }
+            else
+            {
+                CssClass = null;
+                ButtonText = "Hide Footer";
+            }
+
         }
     }
 }
