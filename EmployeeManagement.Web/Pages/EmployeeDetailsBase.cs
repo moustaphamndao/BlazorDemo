59c55764e75c11d2ace203f29a652b0abdacd495
Taphagol <tc1ndao@yahoo.fr>
2020-08-02 19:01:22 -0400

    Implementing Authorization in Component code behind

--- EmployeeManagement.Web/Pages/EmployeeDetailsBase.cs
+++ EmployeeManagement.Web/Pages/EmployeeDetailsBase.cs
@@ -1,10 +1,12 @@
 ﻿using EmployeeManagement.Models;
 using EmployeeManagement.Web.Services;
 using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.AspNetCore.Components.Web;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Security.Permissions;
 using System.Threading.Tasks;
 
@@ -12,6 +14,8 @@ namespace EmployeeManagement.Web.Pages
 {
     public class EmployeeDetailsBase : ComponentBase
     {
+        [CascadingParameter]
+        public Task<AuthenticationState> authenticationStateTask { get; set; }
         public Employee employee { get; set; } = new Employee();
         protected string Coordinates { get; set; }
         protected string ButtonText { get; set; } = "Hide Footer";
@@ -24,8 +28,16 @@ namespace EmployeeManagement.Web.Pages
         [Parameter]
         public string Id { get; set; }
 
+        [Inject]
+        public NavigationManager NavigationManager { get; set; }
         protected async override Task OnInitializedAsync()
         {
+            var authenticationState = await authenticationStateTask;
+            if (!authenticationState.User.Identity.IsAuthenticated)
+            {
+                string returnUrl = WebUtility.UrlEncode($"/employeedetails/{Id}");
+                NavigationManager.NavigateTo($"/identity/account/login?returnUrl={returnUrl}");
+            }
             Id = Id ?? "1"; // In the case the value of Id is not in the URL, Use the empployee with Id 1.
             employee = await employeeService.GetEmployee(int.Parse(Id));
         }
